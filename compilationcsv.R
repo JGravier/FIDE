54d180ba2424117efd5ad0de2291b0cdd9644c52
Julie Gravier <JGravier@users.noreply.github.com>
2023-01-25 09:53:31 +0100

    compilation of csv, output as parquet

---
+++ compilationcsv.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(arrow)
+
+pathcsv <- "Step 4 - Cleaning/Cleaned csvs/"
+pathcsv <- list.files(path = pathcsv, pattern = "*.csv")
+
+tableauranking <- read.csv(file = "Step 4 - Cleaning/Cleaned csvs/APR01.csv", sep = "*") %>%
+  as_tibble()
+
+tableauranking <- tableauranking %>%
+  mutate(estpasvide = "1") %>%
+  filter(estpasvide == 0) %>%
+  select(-estpasvide) %>%
+  select(ID_Number, Name, Title, Country, Rating, Games, Activity, Date) %>%
+  mutate(ID_Number = as.character(ID_Number)) %>%
+  mutate(Games = as.character(Games))
+
+for (i in 1:length(pathcsv)) {
+  
+  tableauimport <- read.csv(file = paste0("Step 4 - Cleaning/Cleaned csvs/", pathcsv[i]), sep = "*") %>%
+    as_tibble() %>%
+    select(ID_Number, Name, Title, Country, Rating, Games, Activity, Date) %>%
+    mutate(ID_Number = as.character(ID_Number)) %>%
+    mutate(Games = as.character(Games)) %>%
+    mutate(dateranking = str_remove_all(string = pathcsv[i], pattern = ".csv"))
+  
+  print(pathcsv[i])
+  
+  tableauranking <- tableauranking %>%
+    bind_rows(tableauimport)
+  
+}
+
+write_parquet(x = tableauranking, sink = "csv_compilations.parquet")
+
